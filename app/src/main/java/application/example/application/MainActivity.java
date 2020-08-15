a083868c93f00a8a778a286aece3981872914057
TeamScoud <nethmi981@gmail.com>
2020-08-15 13:47:30 +0530

    Appoinment first part is finished...

--- app/src/main/java/application/example/application/MainActivity.java
+++ app/src/main/java/application/example/application/MainActivity.java
@@ -2,7 +2,13 @@ package application.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        //creating toast object
+        Context context = getApplicationContext();
+        CharSequence message = "You Just clicked GO Button";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast1 = Toast.makeText(context,message,duration);
+        toast1.show();
+        toast1.setGravity(Gravity.TOP| Gravity.LEFT,0,0);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
